985c2a449b3aa9714610ce472eb134df761c1045
Monte Martin <monte.martin@multipleinc.com>
2019-08-18 20:53:25 -0500

    Got basic (though sloppy) animation working with an SVG

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 // @todo may need to make this close other element (reuse code from isi tabs)
-const lines = document.querySelectorAll(".line");
+const lines = document.querySelectorAll(".callout");
 
 lines.forEach( line => {
     line.addEventListener("click", function() {
-        this.classList.toggle("collapsed");
+        this.classList.toggle("open");
+        this.classList.toggle("closed");
     }, false);
 });
